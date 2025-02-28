c64c1c44d3e8b194c884cd2d4baefff7119524ac
Nikolatrs <sivohnikola97@gmail.com>
2025-03-01 01:01:31 +0300

    changes were made to the code design

--- Assets/tastPole/Scripts/Scale.cs
+++ Assets/tastPole/Scripts/Scale.cs
@@ -1,9 +1,4 @@
-//using System.ComponentModel.Design;
-//using Unity.VisualScripting;
 using UnityEngine;
-//using UnityEngine.Experimental.Rendering;
-//using UnityEngine.UIElements;
-//using static UnityEngine.Rendering.DebugUI;
 
 public class Scale : MonoBehaviour
 {
@@ -23,13 +18,13 @@ public class Scale : MonoBehaviour
     {
         factor = SetCoefficient((Vector3.one * _startSize).magnitude, (Vector3.one * _maxSize).magnitude, transform.localScale.magnitude,factor);
         currentSize += _speedChengeScale * factor;
-        Changeobget(currentSize);
-
+        ChangeObget(currentSize);
     }
 
-    private void Changeobget(float currentSize)
+    private void ChangeObget(float currentSize)
     {
         transform.localScale = Vector3.one * _startSize * currentSize;
+
         if (transform.localScale.x < 0 || transform.localScale.y < 0 || transform.localScale.z < 0) 
             transform.localScale = Vector3.one * _startSize; 
 
@@ -38,7 +33,6 @@ public class Scale : MonoBehaviour
 
         if (transform.localScale.magnitude < (Vector3.one * _startSize).magnitude)
             transform.localScale = Vector3.one * _startSize; 
-
     }
 
     private int SetCoefficient(float min, float max, float Value, int startCoefficient = 1)
@@ -47,6 +41,4 @@ public class Scale : MonoBehaviour
         if (Value <= min) return 1;
         return startCoefficient; 
     }
-
 }
-// ѕункт 6-—ериализованные пол€ от несериализованных отдел€ютс€ пустой строкой. Ќ≈ѕќЌя“Ќќ((( и 7
